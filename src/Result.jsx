4f1492eb7d307e9e18f5d5a78d30ec6bc8c8f0de
Diyari <dearyfareq15@gmail.com>
2023-02-19 23:29:38 +0300

    changed the useState for the shortened link

--- src/Result.jsx
+++ src/Result.jsx
@@ -1,10 +1,17 @@
 import React, { useState } from 'react'
+import CopyToClipboard from 'react-copy-to-clipboard';
 
 const Result = () => {
-    const [shortLink, SetShortLink] = useState("");
+    const [shortLink, SetShortLink] = useState(shortLink);
   return (
     <div className='results'>
         <p>{shortLink}</p>
+        <CopyToClipboard
+        text={shortLink}
+        >
+        <button>Copy to Clipboard</button>
+        </CopyToClipboard>
+        
     </div>
   )
 }
